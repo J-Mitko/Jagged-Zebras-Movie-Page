307d8eadb7f839980818ca1c41445e5c5063ce72
Itilon <ivanov.elect@gmail.com>
2017-09-10 01:42:40 +0300

    About and Footer Component Update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { WindowRef } from './../window.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  document: Document;
+
+  constructor(private winRef: WindowRef) {
+    this.document = document;
+   }
 
   ngOnInit() {
+  }
+
+   @HostListener('window:scroll', ['$event'])
+
+  onScroll(ev) {
+    this.winRef.nativeWindow.requestAnimationFrame(this.scrollHandler);
+  }
+
+  private scrollHandler = () => {
+    if (!this.document.getElementsByClassName('hidden-btn')) {
+      return;
+    }
+    const scrollY = this.winRef.nativeWindow.pageYOffset;
+    const pageHeight = this.document.documentElement.scrollHeight;
+    let btns: any;
+    let btnsArr = Array<any>();
+    if (scrollY >= pageHeight * 52 / 100) {
+      btns = this.document.getElementsByClassName('hidden-btn');
+      btnsArr = Array.from(btns);
+      btnsArr.forEach((btn) => {
+        setTimeout(() => {
+          btn.classList.remove('hidden-btn');
+          btn.classList.add('is-showing');
+        }, 1000);
+      });
+    }
   }
 
 }
