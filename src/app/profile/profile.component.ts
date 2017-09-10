f5d82134dd2e1c00fb625968ab78b241f53024f5
Itilon <ivanov.elect@gmail.com>
2017-09-11 00:43:36 +0300

    Profile Component Additional Update

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth.service';
+import { DocumentRef } from '../shared/document.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,9 +11,15 @@ export class ProfileComponent implements OnInit {
 
   username: '';
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, private docRef: DocumentRef) { }
 
   ngOnInit() {
+  }
+
+  close() {
+    const container = this.docRef.nativeDocument.getElementsByClassName('fb-profile-container')[0];
+    container.classList.remove('fb-profile-container');
+    container.classList.add('fb-profile-container-hidden');
   }
 
 }
